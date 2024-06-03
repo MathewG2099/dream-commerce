6f36bff451fdc96703d9e1d59b471209b7efec88
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-06-03 19:30:12 -0400

    Colums
    -Added Id columns and the category_name columns

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,15 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      allowNull: false, // Disallow NULL values
+      primaryKey: true, // Set as the primary key
+      autoIncrement: true, // Automatically increment the value for each new record
+    },
+    category_name: {
+      type: DataTypes.STRING, // Set the data type to STRING
+    },
   },
   {
     sequelize,
